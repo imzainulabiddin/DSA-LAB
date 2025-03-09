f7991f6fd4d299d653394f3e4f6597e42ea2e708
Zain ul Abiddin <zainulabiddin456@gmail.com>
2025-03-09 16:57:57 +0500

    Singly Linked List (Display Nodes)
    Functions to display the first node, last node, Nth node, and centre node of a singly linked list.

---
+++ task_5.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+        int data;
+        Node* next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node* head;
+    Node* tail;
+    
+    public:
+        List()
+        {
+            head = tail = NULL;
+        }
+        
+        void push_front(int val)
+        {
+            Node* newNode = new Node(val);
+            
+            if(head == NULL)
+            {
+                head = tail = newNode;  
+            }
+            else
+            {
+                newNode->next = head;
+                head = newNode;
+            }
+        }
+        
+        void push_back(int val)
+        {
+            Node* newNode = new Node(val);
+            
+            if(head == NULL)
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+        
+        //Function to insert at any position.
+        void insert(int val, int pos)
+		{
+			if(pos<0)
+			{
+				cerr << "Invalid pos\n";
+				return;
+			}
+			if(pos == 0)
+			{
+				push_front(val);
+				return;
+			}
+			Node* temp = head;
+			for(int i = 0; i<pos-1; i++)
+			{
+				temp = temp->next;
+			}
+			Node* newNode = new Node(val);
+			newNode->next = temp->next;
+			temp->next = newNode;
+		}
+        
+        void DisplayLL()
+        {
+            Node* temp = head;
+            while(temp != NULL)
+            {
+                cout << temp->data << "->";
+                temp = temp->next;
+            }
+            cout << "NULL" << endl; 
+        }
+        
+        // Display the first node
+        void displayFirst() 
+		{
+            if (head)
+                cout << "First Node: " << head->data << endl;
+        }
+
+        // Display the last node
+        void displayLast() 
+		{
+            if (!head) return;
+            Node* temp = head;
+            while(temp->next) 
+			{
+                temp = temp->next;
+            }
+            cout << "Last Node: " << temp->data << endl;
+        }
+
+        // Display the Nth node (0-based index)
+        void displayNth(int n) 
+		{
+            Node* temp = head;
+            int count = 0;
+            while(temp) 
+			{
+                if(count == n) 
+				{
+                    cout << "Nth Node (" << n << "): " << temp->data << endl;
+                    return;
+                }
+                temp = temp->next;
+                count++;
+            }
+            cout << "Invalid position!" << endl;
+        }
+
+        // Display the center node
+        void displayCenter() 
+		{
+            if(!head) return;
+            Node* slow = head;
+            Node* fast = head;
+            while(fast && fast->next) 
+			{
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            cout << "Center Node: " << slow->data << endl;
+        }
+};
+
+int main()
+{
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    
+    ll.DisplayLL();
+    
+    ll.push_back(4);
+    
+    ll.DisplayLL();
+    
+    ll.insert(5, 0);
+    ll.DisplayLL();
+
+    ll.displayFirst();
+    ll.displayLast();
+    ll.displayNth(4); // Display 4nd index node
+    ll.displayCenter();
+
+    return 0;
+}
